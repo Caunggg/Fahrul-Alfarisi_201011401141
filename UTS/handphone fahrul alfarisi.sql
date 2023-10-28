5618a96859a08618fe2236abcfac3bb8d43220d2
Caung <97787874+Caunggg@users.noreply.github.com>
2023-10-28 23:45:54 +0700

    Add files via upload

---
+++ UTS/handphone fahrul alfarisi.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.handphone (brand,ram,processor,rom,baterai,harga) VALUES
+	 ('Samsung Galaxy A24','8 GB','Helio G99','128 GB','5000 mAh','3.000.000'),
+	 ('Samsung Galaxy A34 5G','8 GB','Dimensity 1080 (7nm)','128 GB','5000 mAh','4.125.000'),
+	 ('Samsung Galaxy A54 5G','8 GB','Exynos 1380','128 GB','5100 mAh','4.970.000'),
+	 ('Samsung Galaxy A14 5G','6 GB','Dimensity 700 (7nm)','128 GB','5000 mAh','2.450.000'),
+	 ('Samsung Galaxy A23 5G','6 GB','Snapdragon 695 5G (6nm)','128 GB','5000 mAh','2.550.000'),
+	 ('Samsung Galaxy A04','4 GB','Snapdragon 695 5G (6nm)','64 GB','5000 mAh','1.275.000'),
+	 ('Samsung Galaxy A04s','4 GB','Exynos 850 (8nm)','64 GB','5000 mAh','1.660.000'),
+	 ('Samsung Galaxy A32 5G','8 GB','Dimensity 720G 5G (7nm)','128 GB','5000 mAh','3.200.000'),
+	 ('Samsung Galaxy M23 5G','6 GB','Snapdragon 750G 5G (8nm)','128 GB','5000 mAh','2.595.000'),
+	 ('Samsung Galaxy M33 5G','6 GB','Exynos 1280','128 GB','5000 mAh','3.000.000');
