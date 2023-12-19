ac91176cf63759e2b0368cc8f8190e4e02fa5bb6
Caung <97787874+Caunggg@users.noreply.github.com>
2023-12-19 07:13:13 +0700

    Add files via upload

---
+++ UAS/SPK-Fahrul/settings.py
@@ -0,0 +1,81 @@
+USER = 'postgres'
+PASSWORD = 'bale0101'
+HOST = 'localhost'
+PORT = '5432'
+DATABASE_NAME = 'UAS' 
+
+MEREK_SCALE = {
+  "Samsung Galaxy A24" : 3,
+  "Samsung Galaxy A34 5G" : 2,
+  "Samsung Galaxy A54 5G" : 2,
+  "Samsung Galaxy A14 5G" : 2,
+  "Samsung Galaxy A23 5G" : 4,
+  "Samsung Galaxy A04" : 3,
+  "Samsung Galaxy A04s" : 3,
+  "Samsung Galaxy A32 5G" : 3,
+  "Samsung Galaxy M23 5G" : 4,
+  "Samsung Galaxy M33 5G" : 4
+}
+
+DEV_SCALE_ram = {
+  "2GB" : 2,
+  "4GB" : 3,
+  "6GB" : 3,
+  "8GB" : 4
+}
+
+DEV_SCALE_processor = {
+  "Helio G99" : 3,
+  "Dimensity 1080 (7nm)" : 5,
+  "Exynos 1380" : 5,
+  "Dimensity 700 (7nm)" : 2,
+  "Snapdragon 695 5G (6nm)" : 4,
+  "Exynos 850 (8nm)" : 2,
+  "Dimensity 720G 5G (7nm)" : 2,
+  "Snapdragon 750G 5G (8nm)" : 2,
+  "Exynos 1280" : 4
+}
+
+DEV_SCALE_rom = {
+  "4GB" : 2,
+  "6GB": 2 ,
+  "8GB": 3,
+  "32GB" : 3,
+  "64GB" : 3,
+  "128GB" : 4
+}
+
+DEV_SCALE_baterai = {
+  "5000 mAh" : 4,
+  "5100 mAh" : 5,
+  "4500 mAh" : 3,
+  "4000 mAh" : 2
+}
+
+DEV_SCALE_harga = {
+  "Rp1.275.000" : 2,
+  "Rp1.660.000" : 3,
+  "Rp2.450.000" : 3,
+  "Rp2.550.000" : 3,
+  "Rp2.595.000" : 3,
+  "Rp3.000.000" : 4,
+  "Rp3.100.000" : 4,
+  "Rp3.200.000" : 4,
+  "Rp4.125.000" : 4,
+  "Rp4.970.000" : 5
+
+}
+
+DEV_SCALE_ukuran_layar = {
+  "6.40 inci" : 7,
+  "6.50 inci" : 8,
+  "6.53 inci" : 9,
+  "6.60 inci" : 8,
+  "6.67 inci" : 7,
+  "6.71 inci" : 9
+}
+
+
+
+# https://github.com/agungperdananto/spk_model
+# https://github.com/agungperdananto/SimpleCart
